c4c99019d1757c850695de44e3ba3bdcbcacf46e
Kelsey Ganes <kelsey.ganes@gmail.com>
2016-08-23 17:37:18 -0700

    Updated task controller.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,27 +4,9 @@ class TasksController < ApplicationController
   def new
   end
 
-  # def create
-  #   task = Task.create(task_params)
-  #   respond_to do |format|
-  #     format.html do
-  #       # redirect_to projects_path(task.project.id)
-  #     end
-  #   end
-  # end
-
-  # Task.where("id > 2 AND id < 11").delete_all
   def create
     @task = Task.create(task_params)
     render :json => { message:  "Hello?" }
-    # redirect_to :back
-
-    # if @task.save
-    #   redirect_to "/users/#{current_user.id}"
-    # else
-    #   flash[:errors] = @task.errors.full_messages
-    #     redirect_to :back
-    # end
   end
 
   def destroy
