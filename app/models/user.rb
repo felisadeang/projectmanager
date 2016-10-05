3c6638d1802baaec0650fd9922d4dbfe95b99fe8
Felisa Deang <felisadee@gmail.com>
2016-10-05 14:16:08 -0700

    added password complexity and fixed manager boolean check using inclusion rather than presence check

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,15 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :first_name, :last_name, :department_id, :manager, presence: :true
+  validates :first_name, :last_name, :department_id, presence: true
+  validates :manager, inclusion: { in: [ true, false ] }
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  def password_complexity
+    if password.present? and not password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)./)
+      errors.add :password, "must include at least one lowercase letter, one uppercase letter, and one digit"
+    end
+  end
+  validate :password_complexity
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
   before_save do
     self.email.downcase!
